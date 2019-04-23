6d618ec550dd01986f48077171d24e9e123215cb
Amriesgrace <obieri.grace@gmail.com>
2019-04-24 00:10:31 +0100

    Updates

---
+++ precache-manifest.59f821d5f273817f26bf43511df58b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1faad84b374ef1357a7cd8367ba78d1a",
+    "url": "/starwars/static/media/Logo.1faad84b.png"
+  },
+  {
+    "revision": "95e65c6947d44018319e",
+    "url": "/starwars/static/js/runtime~main.e03e01c4.js"
+  },
+  {
+    "revision": "efe7fb3f56ef9550617b",
+    "url": "/starwars/static/js/main.9ece741b.chunk.js"
+  },
+  {
+    "revision": "5e93ea3ca14b24925692",
+    "url": "/starwars/static/js/2.953e373a.chunk.js"
+  },
+  {
+    "revision": "efe7fb3f56ef9550617b",
+    "url": "/starwars/static/css/main.b035edca.chunk.css"
+  },
+  {
+    "revision": "cee69c465f9bb6972affe1372bb4cb63",
+    "url": "/starwars/index.html"
+  }
+];
